546eb274ad161e25ac4d01e9f313990b2c59fc0d
luminimunasinghe <lumini@engenuityai.lk>
2026-02-22 12:29:06 +0530

    fix swc build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+  swcMinify: false,
+  experimental: {
+    appDir: true,
   },
 }
 
